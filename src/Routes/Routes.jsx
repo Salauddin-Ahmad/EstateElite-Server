2a9fb8ecbd7394a13d1954ae0f093ac1f5f7ee65
Md. Salauddin <salauddinahmed555@gmail.com>
2024-06-01 01:26:50 +0600

    Initialized EstateElite

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../Layouts/Root";
+
+ export const router = createBrowserRouter([
+    {
+     path: '/',
+     element: <Root></Root>,
+     children: [
+        {
+            path: '/',
+        }
+     ]
+    },
+  ]);
