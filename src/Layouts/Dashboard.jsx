18c3cb2fc49c3ea09d82c9b3d4b34390431a0f31
Md. Salauddin <salauddinahmed555@gmail.com>
2024-06-03 18:12:39 +0600

    added agent in dashboard

--- src/Layouts/Dashboard.jsx
+++ src/Layouts/Dashboard.jsx
@@ -1,9 +1,88 @@
+// import { NavLink, Outlet } from "react-router-dom";
+// import { FaHome } from "react-icons/fa";
+// import useAdmin from "../hook/useAdmin";
+
+// const Dashboard = () => {
+//   const [isAdmin] = useAdmin();
+
+//   return (
+//     <div className="flex">
+//       <div className="w-64 min-h-screen bg-orange-200">
+//         <ul className="menu p-4">
+//           {isAdmin ? (
+//             <>
+//               <li>
+//                 <NavLink to="/dashboard/adminProfile">
+//                   <FaHome />
+//                   Admin Profile
+//                 </NavLink>
+//               </li>
+    
+//               <li>
+//                 <NavLink to="/dashboard/manageProperties">
+//                   <FaHome />
+//                   Manage Properties
+//                 </NavLink>
+//               </li>
+//               <li>
+//                 <NavLink to="/dashboard/users">
+//                   <FaHome />
+//                   Manage users
+//                 </NavLink>
+//               </li>
+//               <li>
+//                 <NavLink to="/dashboard/reviews">
+//                   <FaHome />
+//                   Manage reviews
+//                 </NavLink>
+//               </li>
+//             </>
+//           ) : (
+//             <>
+//               <li>
+//                 <NavLink to="/dashboard/userHome">
+//                   <FaHome />
+//                   User Home
+//                 </NavLink>
+//               </li>
+//               <li>
+//                 <NavLink to="/dashboard/userProperties">
+//                   <FaHome />
+//                   All Properties
+//                 </NavLink>
+//               </li>
+//             </>
+//           )}
+//                     {/* shared nav links */}
+//                     <div className="divider"></div>
+//                     <li>
+//                         <NavLink to="/">
+//                             <FaHome></FaHome>
+//                             Home</NavLink>
+//                     </li>
+
+//         </ul>
+//       </div>
+//       <div className="flex-1 p-4">
+//         <Outlet />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Dashboard;
+
+
 import { NavLink, Outlet } from "react-router-dom";
 import { FaHome } from "react-icons/fa";
 import useAdmin from "../hook/useAdmin";
+import useAgent from "../hook/useAgent";
+// import useAuth from "../hook/useAuth";
 
 const Dashboard = () => {
   const [isAdmin] = useAdmin();
+  const [isAgent] = useAgent();
+//   const { user } = useAuth();
 
   return (
     <div className="flex">
@@ -17,7 +96,6 @@ const Dashboard = () => {
                   Admin Profile
                 </NavLink>
               </li>
-    
               <li>
                 <NavLink to="/dashboard/manageProperties">
                   <FaHome />
@@ -27,40 +105,85 @@ const Dashboard = () => {
               <li>
                 <NavLink to="/dashboard/users">
                   <FaHome />
-                  Manage users
+                  Manage Users
                 </NavLink>
               </li>
               <li>
                 <NavLink to="/dashboard/reviews">
                   <FaHome />
-                  Manage reviews
+                  Manage Reviews
+                </NavLink>
+              </li>
+            </>
+          ) : isAgent ? (
+            <>
+              <li>
+                <NavLink to="/dashboard/agentProfile">
+                  <FaHome />
+                  Agent Profile
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/addProperty">
+                  <FaHome />
+                  Add Property
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/myProperties">
+                  <FaHome />
+                  My Added Properties
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/mySoldProperties">
+                  <FaHome />
+                  My Sold Properties
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/requestedProperties">
+                  <FaHome />
+                  Requested Properties
                 </NavLink>
               </li>
             </>
           ) : (
             <>
               <li>
-                <NavLink to="/dashboard/userHome">
+                <NavLink to="/dashboard/userProfile">
                   <FaHome />
-                  User Home
+                  My Profile
                 </NavLink>
               </li>
               <li>
-                <NavLink to="/dashboard/userProperties">
+                <NavLink to="/dashboard/wishlist">
                   <FaHome />
-                  All Properties
+                  Wishlist
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/propertyBought">
+                  <FaHome />
+                  Property Bought
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/myReviews">
+                  <FaHome />
+                  My Reviews
                 </NavLink>
               </li>
             </>
           )}
-                    {/* shared nav links */}
-                    <div className="divider"></div>
-                    <li>
-                        <NavLink to="/">
-                            <FaHome></FaHome>
-                            Home</NavLink>
-                    </li>
-
+          {/* shared nav links */}
+          <div className="divider"></div>
+          <li>
+            <NavLink to="/">
+              <FaHome />
+              Home
+            </NavLink>
+          </li>
         </ul>
       </div>
       <div className="flex-1 p-4">
--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,6 +1,124 @@
-import { useQuery } from "@tanstack/react-query";
+// import { useQuery } from "@tanstack/react-query";
 
-import { FaTrashAlt, FaUsers } from "react-icons/fa";
+// import { FaTrashAlt, FaUsers } from "react-icons/fa";
+// import Swal from "sweetalert2";
+// import useAxiosSecure from "../../../hook/useAxiosSecure";
+
+// const AllUsers = () => {
+//   const axiosSecure = useAxiosSecure();
+//   const { data: users = [], refetch } = useQuery({
+//     queryKey: ["users"],
+//     queryFn: async () => {
+//       const res = await axiosSecure.get("/users");
+//       console.log(res.data);
+//       return res.data;
+//     },
+//   });
+
+//   const handleMakeAdmin = (user) => {
+//     axiosSecure.patch(`/users/admin/${user._id}`).
+//     then((res) => {
+//       console.log(res.data);
+//       if (res.data.modifiedCount > 0) {
+//         refetch();
+//         Swal.fire({
+//           position: "top-end",
+//           icon: "success",
+//           title: `${user.name} is an admin now!`,
+//           showConfirmButton: false,
+//           timer: 1500,
+//         });
+//       }
+//     });
+//   };
+
+//   const handleDeleteUser = (user) => {
+//     Swal.fire({
+//       title: "Are you sure?",
+//       text: "You won't be able to revert this!",
+//       icon: "warning",
+//       showCancelButton: true,
+//       confirmButtonColor: "#3085d6",
+//       cancelButtonColor: "#d33",
+//       confirmButtonText: "Yes, delete it!",
+//     }).then((result) => {
+//       if (result.isConfirmed) {
+//         axiosSecure.delete(`users/${user._id}`).then((res) => {
+//           if (res.data.deletedCount > 0) {
+//             refetch();
+//             Swal.fire({
+//               title: "Deleted!",
+//               text: "Your file has been deleted.",
+//               icon: "success",
+//             });
+//           }
+//         });
+//       }
+//     });
+//   };
+
+//   return (
+//     <div>
+//       <div className="flex justify-evenly my-4">
+//         <h2 className="text-3xl">All Users</h2>
+//         <h2 className="text-3xl">Total Users{users.length}</h2>
+//       </div>
+
+//       <div className="overflow-x-auto">
+//         <table className="table table-zebra w-full">
+//           {/* head */}
+//           <thead>
+//             <tr>
+//               <th></th>
+//               <th>Name</th>
+//               <th>Email</th>
+//               <th>Role</th>
+//               <th>Action</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {users.map((user, index) => (
+//               <tr key={user._id}>
+//                 <th>{index + 1}</th>
+//                 <td>{user.name}</td>
+//                 <td>{user.email}</td>
+//                 <td>
+//                   { user.role === 'admin' ? 'Admin' :  <button
+//                     onClick={() => handleMakeAdmin(user)}
+//                     className="btn btn-lg bg-green-500"
+//                   >
+//                     <FaUsers
+//                       className="text-white font-normal
+//                     text-2xl"
+//                     >
+//                       {" "}
+//                     </FaUsers>
+//                   </button>}
+//                 </td>
+//                 <td>
+//                   <button
+//                     onClick={() => handleDeleteUser(user)}
+//                     className="btn btn-lg bg-red-500"
+//                   >
+//                     <FaTrashAlt className="text-white font-normal text-xs">
+//                       {" "}
+//                     </FaTrashAlt>
+//                   </button>
+//                 </td>
+//               </tr>
+//             ))}
+//           </tbody>
+//         </table>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default AllUsers;
+
+
+import { useQuery } from "@tanstack/react-query";
+import { FaTrashAlt, FaUsers, FaUserTie } from "react-icons/fa";
 import Swal from "sweetalert2";
 import useAxiosSecure from "../../../hook/useAxiosSecure";
 
@@ -16,8 +134,7 @@ const AllUsers = () => {
   });
 
   const handleMakeAdmin = (user) => {
-    axiosSecure.patch(`/users/admin/${user._id}`).
-    then((res) => {
+    axiosSecure.patch(`/users/admin/${user._id}`).then((res) => {
       console.log(res.data);
       if (res.data.modifiedCount > 0) {
         refetch();
@@ -25,6 +142,22 @@ const AllUsers = () => {
           position: "top-end",
           icon: "success",
           title: `${user.name} is an admin now!`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    });
+  };
+
+  const handleMakeAgent = (user) => {
+    axiosSecure.patch(`/users/agent/${user._id}`).then((res) => {
+      console.log(res.data);
+      if (res.data.modifiedCount > 0) {
+        refetch();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${user.name} is an agent now!`,
           showConfirmButton: false,
           timer: 1500,
         });
@@ -61,7 +194,7 @@ const AllUsers = () => {
     <div>
       <div className="flex justify-evenly my-4">
         <h2 className="text-3xl">All Users</h2>
-        <h2 className="text-3xl">Total Users{users.length}</h2>
+        <h2 className="text-3xl">Total Users {users.length}</h2>
       </div>
 
       <div className="overflow-x-auto">
@@ -72,7 +205,7 @@ const AllUsers = () => {
               <th></th>
               <th>Name</th>
               <th>Email</th>
-              <th>Role</th>
+              <th>User Role</th>
               <th>Action</th>
             </tr>
           </thead>
@@ -83,26 +216,33 @@ const AllUsers = () => {
                 <td>{user.name}</td>
                 <td>{user.email}</td>
                 <td>
-                  { user.role === 'admin' ? 'Admin' :  <button
-                    onClick={() => handleMakeAdmin(user)}
-                    className="btn btn-lg bg-green-500"
-                  >
-                    <FaUsers
-                      className="text-white font-normal
-                    text-2xl"
-                    >
-                      {" "}
-                    </FaUsers>
-                  </button>}
+                  {user.role === "admin" ? (
+                    "Admin"
+                  ) : user.role === "agent" ? (
+                    "Agent"
+                  ) : (
+                    <>
+                      <button
+                        onClick={() => handleMakeAdmin(user)}
+                        className="btn btn-lg bg-green-500 mr-2"
+                      >
+                        <FaUsers className="text-white font-normal text-2xl" /> Admin
+                      </button>
+                      <button
+                        onClick={() => handleMakeAgent(user)}
+                        className="btn btn-lg bg-blue-500"
+                      >
+                        <FaUserTie className="text-white font-normal text-2xl" /> Agent
+                      </button>
+                    </>
+                  )}
                 </td>
                 <td>
                   <button
                     onClick={() => handleDeleteUser(user)}
                     className="btn btn-lg bg-red-500"
                   >
-                    <FaTrashAlt className="text-white font-normal text-xs">
-                      {" "}
-                    </FaTrashAlt>
+                    <FaTrashAlt className="text-white font-normal text-xs" />
                   </button>
                 </td>
               </tr>
